4678d6144c3eea7b497685977223580739236b3e
sahajavidya <sahaja.vidya.channel@gmail.com>
2020-05-10 00:19:42 +0530

    added logrus

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sahajavidya/learngows
 
 go 1.14
 
-require github.com/sahajavidya/mathutils v0.0.0-20200502141605-5fae07df7c8f
+require (
+	github.com/sahajavidya/mathutils v0.0.0-20200502141605-5fae07df7c8f
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 
-   	"github.com/sahajavidya/learngows/stringutils"
+	"github.com/sahajavidya/learngows/stringutils"
+	"github.com/sahajavidya/mathutils"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	s := "sahajavidya"
 
 	fmt.Println(stringutils.Upper(s))
+	fmt.Println("Addition of two Number:", mathutils.Add(1, 4))
+	log.Info("Using Third Party package")
+
 }
